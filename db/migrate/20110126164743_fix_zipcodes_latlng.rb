317a2af8d8a30b12478e3f42de18d841b3bb4711
iancrystal <ian_crystal@yahoo.com>
2011-01-27 10:59:47 -0800

    update fix zipcodes migration

--- db/migrate/20110126164743_fix_zipcodes_latlng.rb
+++ db/migrate/20110126164743_fix_zipcodes_latlng.rb
@@ -9,8 +9,11 @@ class FixZipcodesLatlng < ActiveRecord::Migration
     
     #File.open("zipcodes.log","w") do |log|
     
+      fail_count = 0
+      prev_fail = false
+
       #for i in 1..41329
-      for i in 1..41329
+      for i in 10200..41329
         puts i
         #log.puts i
         begin
@@ -23,6 +26,9 @@ class FixZipcodesLatlng < ActiveRecord::Migration
       
         if g
           if (z.lat && z.lng && g.lat && g.lng)
+            fail_count = 0
+            prev_fail = false
+
             latdiff = (z.lat - g.lat).abs
             lngdiff = (z.lng - g.lng).abs
             if ( latdiff > 0.0001 || lngdiff > 0.0001 )
@@ -33,6 +39,12 @@ class FixZipcodesLatlng < ActiveRecord::Migration
               z.save
             end
           else
+            if prev_fail
+              fail_count += 1
+            else
+              prev_fail = true
+            end
+            exit if fail_count == 5
             puts "z.lat=#{z.lat.to_s}, z.lng=#{z.lng.to_s}, g.lat=#{g.lat.to_s}, g.lng=#{g.lng.to_s}"
             #log.puts "z.lat=#{z.lat.to_s}, z.lng=#{z.lng.to_s}, g.lat=#{g.lat.to_s}, g.lng=#{g.lng.to_s}"
           end
@@ -49,3 +61,4 @@ class FixZipcodesLatlng < ActiveRecord::Migration
   def self.down
   end
 end
+
