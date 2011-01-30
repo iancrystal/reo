83bcec31cf7304e6b966d9b4152277446cb5c8ba
iancrystal <ian_crystal@yahoo.com>
2011-01-29 16:33:07 -0800

    update README

--- db/migrate/20110126164743_fix_zipcodes_latlng.rb
+++ db/migrate/20110126164743_fix_zipcodes_latlng.rb
@@ -13,7 +13,7 @@ class FixZipcodesLatlng < ActiveRecord::Migration
       prev_fail = false
 
       #for i in 1..41329
-      for i in 10960..41329
+      for i in 11330..41329
         puts i
         #log.puts i
         begin
