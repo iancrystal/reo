a56c27519b56126cc06b7076fc688a75ff11a9df
iancrystal <ian_crystal@yahoo.com>
2011-02-01 22:28:55 -0800

    fix rmagick

--- config/environment.rb
+++ config/environment.rb
@@ -42,6 +42,4 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   # config.gem "geokit"
-  config.gem "rmagick", :lib => "RMagick" 
-
 end
